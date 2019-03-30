193a08ed37609b5419c663cb387ee09fd470038f
conatuseus <conatuseus@gmail.com>
2019-03-30 16:06:26 +0900

    Fix AppView class non-static to static
    AppView class don't need non-static instance,
    so exchange non-static to static.

--- src/main/java/com/conatuseus/racingcar/appview/AppView.java
+++ src/main/java/com/conatuseus/racingcar/appview/AppView.java
@@ -2,7 +2,7 @@ package com.conatuseus.racingcar.appview;
 
 import java.util.Scanner;
 
-public class AppView {
+public final class AppView {
 
     private static Scanner sc=new Scanner(System.in);
 
