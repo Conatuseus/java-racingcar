ab1090c732722f78da4fb92d65fe29196f3990fe
conatuseus <conatuseus@gmail.com>
2019-03-30 15:56:35 +0900

    Create RacingCar class, Add basic method

--- src/main/java/com/conatuseus/racingcar/appview/AppView.java
+++ src/main/java/com/conatuseus/racingcar/appview/AppView.java
@@ -1,4 +1,16 @@
 package com.conatuseus.racingcar.appview;
 
+import java.util.Scanner;
+
 public class AppView {
+
+    private static Scanner sc=new Scanner(System.in);
+
+    private AppView(){
+    }
+
+    public void outputLine(String aString){
+        System.out.println(aString);
+    }
+
 }
