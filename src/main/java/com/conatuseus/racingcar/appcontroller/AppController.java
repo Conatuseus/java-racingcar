c987a42177e9355f51e5c0086cd30ef733a82d40
conatuseus <conatuseus@gmail.com>
2019-03-31 15:17:06 +0900

    Create inputAndMakeCarList method, Add Car's nmae getter

--- src/main/java/com/conatuseus/racingcar/appcontroller/AppController.java
+++ src/main/java/com/conatuseus/racingcar/appcontroller/AppController.java
@@ -1,8 +1,13 @@
 package com.conatuseus.racingcar.appcontroller;
 
 import com.conatuseus.racingcar.appview.AppView;
+import com.conatuseus.racingcar.model.Car;
+
+import java.util.ArrayList;
 
 public class AppController {
+
+    public static ArrayList<Car> carList=new ArrayList<>();
 
     public AppController(){
     }
@@ -11,13 +16,18 @@ public class AppController {
     public void run(){
         AppView.outputLine(">> 게임을 시작합니다.");
 
+        this.inputAndMakeCarList();
+
+
         AppView.outputLine("<< 게임을 종료합니다.");
     }
 
     private void inputAndMakeCarList(){
         String[] carsName=AppView.inputNameOfCar();
 
-
+        for(String name : carsName){
+            carList.add(new Car(name));
+        }
     }
 
 }
