f15db6c674a1e6418f7925c2a7047e69510927a8
conatuseus <conatuseus@gmail.com>
2019-03-31 16:20:50 +0900

    Add max,min of RANDOM value

--- src/main/java/com/conatuseus/racingcar/model/Car.java
+++ src/main/java/com/conatuseus/racingcar/model/Car.java
@@ -1,6 +1,9 @@
 package com.conatuseus.racingcar.model;
 
 public class Car {
+    private static final int MAX_RANDOM=9;
+    private static final int MIN_RANDOM=0;
+
     private final String name;
     private int position = 0;
 
